d05dacc73477454a515eb0e544091a18313f40d8
Dametrious Peyton <dametrious.peyton@raritan.com>
2014-05-29 11:03:30 -0400

    refactoring: updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo /usr/local/bin/petn9
-sudo /usr/local/include/petn9/manager.h
-sudo /usr/local/include/petn9/databasemanager.h
-sudo /usr/local/include/petn9/petn9.h
-sudo /usr/local/include/petn9/declarativelist.h
-sudo /usr/local/include/petn9/declarativelist_global.h
-sudo /usr/local/share/applications/petn9.desktop
-sudo /usr/local/share/icons/hicolor/scalable/apps/petn9.svg
-
+rm /usr/local/bin/petn9
+rm /usr/local/include/petn9/manager.h
+rm /usr/local/include/petn9/databasemanager.h
+rm /usr/local/include/petn9/petn9.h
+rm /usr/local/include/petn9/declarativelist.h
+rm /usr/local/include/petn9/declarativelist_global.h
+rm /usr/local/share/applications/petn9.desktop
+rm /usr/local/share/icons/hicolor/scalable/apps/petn9.svg
+rmdir /usr/local/include/petn9
