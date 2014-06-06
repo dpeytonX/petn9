712396ca82cb300bf6d285b1fe1d76f719815242
Dametrious Peyton <dametrious.peyton@raritan.com>
2014-06-06 19:02:23 -0400

    kde5: initial import
    Using KF5 KDeclarative QmlObject

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+#include <petn9.h>
+
+#if PETN9_KDE > 0
+#include <KDeclarative/QmlObject>
+#endif
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml>
@@ -7,16 +13,15 @@
 #include <QString>
 #include <QLocale>
 
-#include <petn9.h>
 #include <pet.h>
 #include <spritemodel.h>
-
 #include "manager.h"
 #include "appsettings.h"
 
 Q_DECL_EXPORT int main(int argc, char* argv[]) {
     QScopedPointer<QGuiApplication> app(new QGuiApplication ( argc, argv ));
     QTranslator translator;
+
 
 #if ALLOW_JP > 0
     qDebug() << "Loading Japanese translation";
@@ -32,24 +37,37 @@ Q_DECL_EXPORT int main(int argc, char* argv[]) {
     }
 #endif
     app->installTranslator(&translator);
+    app->setApplicationDisplayName(app->tr("Pet N9"));
+    app->setApplicationVersion(PETN9_VERSION_MAJOR + "." + PETN9_VERSION_MINOR);
 
-    QQmlApplicationEngine viewer;
+#if PETN9_KDE > 0
+    KDeclarative::QmlObject* context = new KDeclarative::QmlObject(app.data());
+    QQmlEngine* viewer = (context->engine());
+#else
+    QQmlApplicationEngine* viewer = new QQmlApplicationEngine(app.data());
+#endif
+
     //Startup context
     QTime time = QTime::currentTime(); //initialize random numbers
     qsrand((uint)time.msec());
 
-    Manager* manager = new Manager(&viewer);
-    AppSettings* appSettings = new AppSettings(&viewer);
+    Manager* manager = new Manager(viewer);
+    AppSettings* appSettings = new AppSettings(viewer);
 
     //Set context variables
     qmlRegisterType<Pet>("com.blogspot.iamboke", 1,0, "PetModel");
     qmlRegisterType<SpriteModel>("com.blogspot.iamboke", 1,0, "SpriteModel");
     qRegisterMetaType<PetStatus>("PetStatus");
 
-    viewer.rootContext()->setContextProperty("Manager", manager);
-    viewer.rootContext()->setContextProperty("AppSettings", appSettings);
+    viewer->rootContext()->setContextProperty("Manager", manager);
+    viewer->rootContext()->setContextProperty("AppSettings", appSettings);
 
-    viewer.load ( QUrl ( QStringLiteral ( "qrc:///qml/qt5/main.qml" ) ) );
+    QUrl qurl( QStringLiteral ( "qrc:///qml/qt5/main.qml" ) );
+#if PETN9_KDE > 0
+    context->createObjectFromSource (qurl, new QQmlContext(viewer));
+#else
+    viewer->load (qurl );
+#endif
 
     return app->exec();
 }
